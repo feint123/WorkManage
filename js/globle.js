8c405488346ff10af41023380c8823b5bf6c9fab
feint123 <1084645640@qq.com>
2017-02-27 21:40:03 +0800

    修改

--- js/globle.js
+++ js/globle.js
@@ -4,3 +4,16 @@
 function workPath(relaPath){
     return "http://120.77.34.140:8080/"+relaPath;
 }
+String.prototype.replaceAll=function(oldString,newString){
+    return this.replace(new RegExp(oldString,"gm"),newString);
+}
+
+String.prototype.paragraph=function () {
+    var paras=this.split("\n");
+    var result="";
+    paras.forEach(function (para) {
+        result=result+"<p>"+para+"</p>";
+    })
+
+    return result;
+}
